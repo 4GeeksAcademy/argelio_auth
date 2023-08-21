a1c52c878227b436e71c5b333f6ccbbd8d67fcf2
Argelio Baca <49691196+Argeliosk8@users.noreply.github.com>
2023-08-21 00:35:06 +0000

    created a protected route

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 from flask_bcrypt import Bcrypt
 
 
@@ -62,3 +64,11 @@ def register():
         return jsonify({"message": "the new user was added"})
     else:
         return jsonify({"message": "error al ingresar el usuario"})
+    
+@api.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user_email = get_jwt_identity()
+    user = User.query.filter_by(email = current_user_email).first()
+    
+    return jsonify({"email": user.email}), 200
